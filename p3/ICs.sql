62dccb1a553c61f4e9585bf3972e5e11bfdc9955
João Rocha <joao.tfrocha@protonmail.com>
2022-06-16 16:32:28 +0100

    Review of Integrity Constraints SQL

--- p3/ICs.sql
+++ p3/ICs.sql
@@ -1,13 +1,27 @@
-%IC-1
+--IC-1
 
 CREATE OR REPLACE FUNCTION check_category_loop()
 RETURNS TRIGGER AS
 $$
+DECLARE cat_name VARCHAR(255);
 BEGIN
-    IF NEW.category = NEW.super_category
-        RAISE EXCEPTION 'A category can not be contained it itself'
-    END IF
-
+    SET cat_name := NEW.super_category
+    LOOP
+        IF NEW.category = cat_name
+            RAISE EXCEPTION 'A category can not be contained it itself'
+        ELSEIF cat_name IN (
+            SELECT category
+            FROM has_other
+        )
+            SET cat_name := (
+                SELECT super_category
+                FROM has_other
+                WHERE category = cat_name
+            )
+        ELSE
+            EXIT
+        END IF
+    END LOOP
     RETURN NEW
 END
 $$ LANGUAGE plpgsql
@@ -16,52 +30,67 @@ CREATE TRIGGER check_category_loop_trigger
 BEFORE UPDATE OR INSERT ON has_other
 FOR EACH ROW EXECUTE PROCEDURE check_category_loop()
 
-%IC-4
+--IC-4
 
-CREATE OR REPLACE FUNCTION check_replenished_units(units INTEGER, serial_number CHAR)
+CREATE OR REPLACE FUNCTION check_replenished_units()
 RETURNS TRIGGER AS
 $$
 DECLARE max_units INTEGER
 BEGIN
-    FROM planogram 
-    SELECT units INTO max_units
-    WHERE planogram.serial_number = serial_number
+    SET max_units := (
+        SELECT units
+        FROM planogram 
+        WHERE ean = NEW.ean 
+            AND nr = NEW.nr
+            AND serial_number = NEW.serial_number
+            AND producer = NEW.producer
+    )
 
-    IF units > max_units
+    IF NEW.units > max_units
         RAISE EXCEPTION 'The number of replenished units from a resupply event
         can not exceed the number of units specified in the planogram'
     END IF
 
-    RETURN %???
+    RETURN NEW
 END
 $$ LANGUAGE plpgsql
 
 
 CREATE TRIGGER replenished_units_trigger
-BEFORE UPDATE OR INSERT ON resuply_event
-FOR EACH ROW EXECUTE PROCEDURE check_replenished_units(resuply_event.units, resuply_event.serial_number)
+BEFORE INSERT ON resuply_event
+FOR EACH ROW EXECUTE PROCEDURE check_replenished_units()
 
-%IC-5
+--IC-5
 
-CREATE OR REPLACE FUNCTION heck_replenished_product(serial_nr CHAR, p_ean CHAR)
+CREATE OR REPLACE FUNCTION check_replenished_product()
 RETUNS TRIGGER AS
 $$
-DECLARE shelf_nr INTEGER
+DECLARE prod_cat VARCHAR(255);
+DECLARE shelf_cat VARCHAR(255);
 BEGIN
-    FROM shelf
-    NATURAL JOIN planogram
-    SELECT number INTO shelf_nr
-    WHERE serial_number = serial_nr AND ean = p_ean
+    SET prod_cat = (
+        SELECT name
+        FROM has_category
+        WHERE ean = NEW.ean
+    )
 
-    IF p_ean.cat != shelf_nr.name
+    SET shelf_cat = (
+        SELECT name
+        FROM shelf
+        WHERE nr = NEW.nr 
+            AND serial_number = NEW.serial_number
+            AND producer = NEW.producer
+    )
+
+    IF prod_cat != shelf_cat
         RAISE EXCEPTION 'A product can only be replenished in a shelf that shows
         at least one of that products category'
     END IF
 
-    RETURN %???
+    RETURN NEW
 END
 $$ LANGUAGE plpgsql
 
 CREATE TRIGGER replenished_product_trigger
-BEFORE UPDATE OR INSERT ON resuply_event
-FOR EACH ROW EXECUTE PROCDEURE check_replenished_product(resuply_event.serial_number, resuply_event.ean)
+BEFORE INSERT ON resuply_event
+FOR EACH ROW EXECUTE PROCDEURE check_replenished_product()
