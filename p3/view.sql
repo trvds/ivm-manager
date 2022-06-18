c64749adfb7d168a4d41e53ea96535693c9c34e9
d-melita <dgmelita9@gmail.com>
2022-06-18 12:13:04 +0100

    small fixes to view and olaps regarding month and day_month issue

--- p3/view.sql
+++ p3/view.sql
@@ -1,11 +1,12 @@
 DROP VIEW IF EXISTS Vendas;
 
 CREATE VIEW 
-    Vendas(ean, cat, year, quarter, month_day, week_day, district, county, units)
+    Vendas(ean, cat, year, quarter, month, month_day, week_day, district, county, units)
 AS
 SELECT ean, name AS cat, 
     EXTRACT(YEAR FROM instant) AS year,
     EXTRACT(QUARTER FROM instant) AS quarter,
+    EXTRACT(MONTH FROM instant) AS month,
     EXTRACT(DAY FROM instant) AS month_day,
     EXTRACT(DOW FROM instant) AS week_day,
     district, county, units
