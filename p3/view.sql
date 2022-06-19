19ad296d7b82a1ffb6be1b7923e985c6d9c6fae2
trvds <me@trvds.pt>
2022-06-19 16:16:36 +0100

    Truly fixed view.sql

--- p3/view.sql
+++ p3/view.sql
@@ -3,11 +3,11 @@ DROP VIEW IF EXISTS sales;
 CREATE VIEW 
     sales(ean, cat, year, quarter, month, month_day, week_day, district, county, units)
 AS
-SELECT ean, name AS cat, 
+SELECT ean, has_category.name AS cat, 
     EXTRACT(YEAR FROM instant) AS year,
     EXTRACT(QUARTER FROM instant) AS quarter,
     EXTRACT(MONTH FROM instant) AS month,
     EXTRACT(DAY FROM instant) AS month_day,
     EXTRACT(DOW FROM instant) AS week_day,
     district, county, units
-FROM resupply_event NATURAL JOIN retail_point NATURAL JOIN has_other
+FROM resupply_event NATURAL JOIN has_category NATURAL JOIN installed_in INNER JOIN retail_point ON installed_in.local = retail_point.name;
