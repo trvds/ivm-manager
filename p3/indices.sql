7d91a3acb4ff59bbb9405a69762387624d86fbfc
João Rocha <joao.tfrocha@protonmail.com>
2022-06-22 18:55:32 +0100

    Organizing project files

--- p3/indices.sql
+++
@@ -1,7 +0,0 @@
---7.1
-CREATE INDEX resp_cat_idx ON responsible_for(category_name) INCLUDE (tin);
-CREATE INDEX retailer_idx ON retailer(tin) INCLUDE (name);
-
---7.2
-CREATE INDEX prod_desc_idx ON product(descr) INCLUDE (cat);
-CREATE INDEX has_cat_name_idx ON has_category(name) INCLUDE (ean);
