370fd30168baf56b571792d8a7ee3c7279eb526c
João Rocha <joao.tfrocha@protonmail.com>
2022-06-23 19:42:43 +0100

    Specify attribute table on view

--- p3/view.sql
+++ p3/view.sql
@@ -3,11 +3,15 @@ DROP VIEW IF EXISTS sales;
 CREATE VIEW 
     sales(ean, cat, year, quarter, month, month_day, week_day, district, county, units)
 AS
-SELECT ean, has_category.name AS cat, 
-    EXTRACT(YEAR FROM instant) AS year,
-    EXTRACT(QUARTER FROM instant) AS quarter,
-    EXTRACT(MONTH FROM instant) AS month,
-    EXTRACT(DAY FROM instant) AS month_day,
-    EXTRACT(ISODOW FROM instant) AS week_day,
-    district, county, units
-FROM resupply_event NATURAL JOIN has_category NATURAL JOIN installed_in INNER JOIN retail_point ON installed_in.local = retail_point.name;
+SELECT resupply_event.ean, has_category.name AS cat, 
+    EXTRACT(YEAR FROM resupply_event.instant) AS year,
+    EXTRACT(QUARTER FROM resupply_event.instant) AS quarter,
+    EXTRACT(MONTH FROM resupply_event.instant) AS month,
+    EXTRACT(DAY FROM resupply_event.instant) AS month_day,
+    EXTRACT(ISODOW FROM resupply_event.instant) AS week_day,
+    retail.point.district, retail.point.county, resupply_event.units
+FROM resupply_event 
+    NATURAL JOIN has_category 
+    NATURAL JOIN installed_in 
+    INNER JOIN retail_point 
+    ON installed_in.local = retail_point.name;
