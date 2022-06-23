e8d5075321dbb7192bdd8878b259073a27bd0dff
João Rocha <joao.tfrocha@protonmail.com>
2022-06-23 16:32:24 +0100

    Further code formatting on schema.sql

--- p3/schema.sql
+++ p3/schema.sql
@@ -16,57 +16,61 @@ DROP TABLE IF EXISTS resupply_event cascade;
 
 CREATE TABLE category (
     name VARCHAR(255) NOT NULL,
-    constraint pk_category primary key(name)
+    CONSTRAINT pk_category PRIMARY KEY(name)
 );
 
 CREATE TABLE simple_category (
     name VARCHAR(255) NOT NULL,
-    constraint pk_simple_category primary key(name),
-    constraint fk_simple_category foreign key(name) references category(name)
+    CONSTRAINT pk_simple_category PRIMARY KEY(name),
+    CONSTRAINT fk_simple_category FOREIGN KEY(name) references category(name)
 );
 
 CREATE TABLE super_category (
     name VARCHAR(255) NOT NULL,
-    constraint pk_super_category primary key(name),
-    constraint fk_super_category foreign key(name) references category(name)
+    CONSTRAINT pk_super_category PRIMARY KEY(name),
+    CONSTRAINT fk_super_category FOREIGN KEY(name) references category(name)
 );
 
 CREATE TABLE has_other (
     super_category VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
     CHECK (super_category != category),
-    constraint pk_has_other primary key(category),
-    constraint fk_has_other_super_category foreign key(super_category) references super_category(name),
-    constraint fk_has_other_category foreign key(category) references category(name)
+    CONSTRAINT pk_has_other PRIMARY KEY(category),
+    CONSTRAINT fk_has_other_super_category FOREIGN KEY(super_category) 
+        references super_category(name),
+    CONSTRAINT fk_has_other_category FOREIGN KEY(category) 
+        references category(name)
 );
 
 CREATE TABLE product (
     ean CHAR(13) NOT NULL,
     cat VARCHAR(255) NOT NULL,
     descr VARCHAR(255) NOT NULL,
-    constraint pk_product primary key(ean),
-    constraint fk_product_category foreign key(cat) references category(name)
+    CONSTRAINT pk_product PRIMARY KEY(ean),
+    CONSTRAINT fk_product_category FOREIGN KEY(cat) references category(name)
 );
 
 CREATE TABLE has_category(
     ean CHAR(13) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    constraint pk_has_category primary key(ean, name),
-    constraint fk_has_category_product foreign key(ean) references product(ean),
-    constraint fk_has_category_category foreign key(name) references category(name)
+    CONSTRAINT pk_has_category PRIMARY KEY(ean, name),
+    CONSTRAINT fk_has_category_product FOREIGN KEY(ean) 
+        references product(ean),
+    CONSTRAINT fk_has_category_category FOREIGN KEY(name) 
+        references category(name)
 );
 
 CREATE TABLE ivm(
     serial_number CHAR(20) NOT NULL,
     manuf VARCHAR(255) NOT NULL,
-    constraint pk_ivm primary key(serial_number, manuf)
+    CONSTRAINT pk_ivm PRIMARY KEY(serial_number, manuf)
 );
 
 CREATE TABLE retail_poINT (
     name VARCHAR(255) NOT NULL,
     district VARCHAR(255) NOT NULL,
     county VARCHAR(255) NOT NULL,
-    constraint pk_retail_point primary key(name)
+    CONSTRAINT pk_retail_point PRIMARY KEY(name)
 );
 
 
@@ -74,9 +78,11 @@ CREATE TABLE installed_in (
     serial_number CHAR(20) NOT NULL,
     manuf VARCHAR(255) NOT NULL,
     local VARCHAR(255) NOT NULL,
-    constraint pk_installed_in primary key(serial_number, manuf),
-    constraint fk_installed_in_ivm foreign key(serial_number, manuf) references ivm(serial_number, manuf),
-    constraint fk_installed_in_retail_point foreign key(local) references retail_point(name)
+    CONSTRAINT pk_installed_in PRIMARY KEY(serial_number, manuf),
+    CONSTRAINT fk_installed_in_ivm FOREIGN KEY(serial_number, manuf) 
+        references ivm(serial_number, manuf),
+    CONSTRAINT fk_installed_in_retail_point FOREIGN KEY(local) 
+        references retail_point(name)
 );
 
 CREATE TABLE shelf (
@@ -85,9 +91,10 @@ CREATE TABLE shelf (
     manuf VARCHAR(255) NOT NULL,
     height real NOT NULL,
     name VARCHAR(255) NOT NULL,
-    constraint pk_shelf primary key(number, serial_number, manuf),
-    constraint fk_shelf_ivm foreign key(serial_number, manuf) references ivm(serial_number, manuf),
-    constraint fk_shelf_category foreign key(name) references category(name)
+    CONSTRAINT pk_shelf PRIMARY KEY(number, serial_number, manuf),
+    CONSTRAINT fk_shelf_ivm FOREIGN KEY(serial_number, manuf) 
+        references ivm(serial_number, manuf),
+    CONSTRAINT fk_shelf_category FOREIGN KEY(name) references category(name)
 );
 
 CREATE TABLE planogram (
@@ -98,15 +105,16 @@ CREATE TABLE planogram (
     faces INT NOT NULL,
     units INT NOT NULL,
     loc INT NOT NULL,
-    constraint pk_planogram primary key(ean, number, serial_number, manuf),
-    constraint fk_planogram_product foreign key(ean) references product(ean),
-    constraint fk_planogram_shelf foreign key(number, serial_number, manuf) references shelf(number, serial_number, manuf)
+    CONSTRAINT pk_planogram PRIMARY KEY(ean, number, serial_number, manuf),
+    CONSTRAINT fk_planogram_product FOREIGN KEY(ean) references product(ean),
+    CONSTRAINT fk_planogram_shelf FOREIGN KEY(number, serial_number, manuf) 
+        references shelf(number, serial_number, manuf)
 );
 
 CREATE TABLE retailer (
     tin CHAR(9) NOT NULL,
     name VARCHAR(255) NOT NULL unique,
-    constraint pk_retailer primary key(tin)
+    CONSTRAINT pk_retailer PRIMARY KEY(tin)
 );
 
 CREATE TABLE responsible_for (
@@ -114,10 +122,13 @@ CREATE TABLE responsible_for (
     tin CHAR(9) NOT NULL,
     serial_number CHAR(20) NOT NULL,
     manuf VARCHAR(255) NOT NULL,
-    constraint pk_responsible_for primary key(serial_number, manuf),
-    constraint fk_responsible_for_ivm foreign key(serial_number, manuf) references ivm(serial_number, manuf),
-    constraint fk_responsible_for_retailer foreign key(tin) references retailer(tin),
-    constraint fk_responsible_for_category foreign key(category_name) references category(name)
+    CONSTRAINT pk_responsible_for PRIMARY KEY(serial_number, manuf),
+    CONSTRAINT fk_responsible_for_ivm FOREIGN KEY(serial_number, manuf) 
+        references ivm(serial_number, manuf),
+    CONSTRAINT fk_responsible_for_retailer FOREIGN KEY(tin) 
+        references retailer(tin),
+    CONSTRAINT fk_responsible_for_category FOREIGN KEY(category_name) 
+        references category(name)
 );
 
 CREATE TABLE resupply_event(
@@ -128,7 +139,8 @@ CREATE TABLE resupply_event(
     instant TIMESTAMP NOT NULL,
     units INT NOT NULL,
     tin CHAR(9) NOT NULL,
-    constraint pk_resuply_event primary key(ean, number, serial_number, manuf, instant),
-    constraint fk_resuply_event_planogram foreign key(ean, number, serial_number, manuf) references planogram(ean, number, serial_number, manuf),
-    constraint fk_resuply_event_retailer foreign key(tin) references retailer(tin)
+    CONSTRAINT pk_resuply_event PRIMARY KEY(ean, number, serial_number, manuf, instant),
+    CONSTRAINT fk_resuply_event_planogram FOREIGN KEY(ean, number, serial_number, manuf) 
+        references planogram(ean, number, serial_number, manuf),
+    CONSTRAINT fk_resuply_event_retailer FOREIGN KEY(tin) references retailer(tin)
 );
